e4e29165b056690b89e764c6089012f07a7aae72
Michele Chilanti <chilantim@gmail.com>
2020-02-12 15:22:35 -0600

    added PVC and setup script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ oc adm policy add-scc-to-group privileged system:serviceaccounts:appsody-k8s
 oc adm policy add-scc-to-group anyuid system:serviceaccounts:appsody-k8s
 oc apply -f ./service-account.yaml
 oc apply -f ./cluster-role-binding.yaml
+oc apply -f ./pvc.yaml
